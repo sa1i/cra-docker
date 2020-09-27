5f723689ebbb84bf814f9ea76cb39c05836df42b
Sai <sai@dodoex.io>
2020-09-28 01:15:22 +0800

    update docker config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-echo "window.ENV = `jo \`env | grep REACT_APP_ | sed -e 's/[[:blank:]]/{_!!_}/g'\` end=1`" | sed -e 's/{_!!_}/ /g' > env.js
+echo "window.env = `jo \`env | grep REACT_APP_ | sed -e 's/[[:blank:]]/{_!!_}/g'\` end=1`" | sed -e 's/{_!!_}/ /g' > env.js
 nginx -g "daemon off;"
